09e72330845b0d39389390af11cb1e946d8e2c2c
leowilson <leobca2018@gmail.com>
2022-07-21 06:33:13 +0530

    add-some-codes.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var table = document.querySelector('.table-class');
 console.log(table);
 var tableObject = [
     {
-        img: 'pic',
+        img: "https://image.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1768126784.jpg",
         name: "Vimal",
         mobile: 8389289819,
         batch: 'FSD-10',
@@ -14,8 +14,8 @@ var tableObject = [
         placement: 'Yes'
     },
     {
-        img: 'pic',
-        name: "Mari-selvam",
+        img: 'https://media.gettyimages.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?s=612x612',
+        name: "Mariselvam",
         mobile: 83892828319,
         batch: 'FSD-10',
         git: [
@@ -26,7 +26,7 @@ var tableObject = [
         placement: 'No'
     },
     {
-        img: 'pic',
+        img: 'https://image.shutterstock.com/image-photo/image-happy-brunette-man-wearing-260nw-1489874846.jpg',
         name: "Leowilson",
         mobile: 7339212569,
         batch: 'FSD-10',
@@ -38,44 +38,49 @@ var tableObject = [
         placement: 'No'
     },
     {
-        img: 'pic',
+        img: 'selvasir.jpeg',
         name: "Selva Mariyappan",
         mobile: 8270056940,
         batch: 'Fab',
         git: [
-            link = 'https://github.com/'
+            link = 'https://github.com/selva06092020'
         ],
         referby: "Vel",
         location: 'Thippanampatti',
         placement: 'Yes'
     }
 ]
-
 // check array length
 var x = (tableObject).length;
-// console.log(x);
 
-// append td in table
+// append dynamic tabledata in the table
 var y;
 var tableRow = document.createElement('tr');
+var tData = document.createElement('td');
 function tableData() {
     for (i = 0; i < tableObject.length; i++) {
         tableRow = document.createElement('tr');
+
         for (j = 0; j < Object.keys(tableObject[i]).length; j++) {
-            if (j == 5) {
+            if( j == 1){
+                var imgEle = document.createElement('img');
+                imgEle.setAttribute('src',tableObject[i].img)
+                tData.appendChild(imgEle);
+            }
+           else if (j == 5) {
                 var linkEle = document.createElement('a');
                 linkEle.setAttribute('href', tableObject[i].git);
                 linkEle.setAttribute('target', 'blank');
                 linkEle.innerHTML = 'Link';
-                placementRow.appendChild(linkEle);
-                y = a.splice(4, 0, linkEle);
+                tData.appendChild(linkEle);
             }
-            var a = [tableObject[i].img, tableObject[i].name, tableObject[i].mobile, tableObject[i].batch, "", tableObject[i].referby, tableObject[i].location, tableObject[i].placement];
-            var placementRow = document.createElement('td');
-            placementRow.innerHTML = a[j];
-            tableRow.appendChild(placementRow);
+            var a = ['', tableObject[i].name, tableObject[i].mobile, tableObject[i].batch, "", tableObject[i].referby, tableObject[i].location, tableObject[i].placement];
+            tData = document.createElement('td');
+            tData.innerHTML = a[j];
+            tableRow.appendChild(tData);
             table.appendChild(tableRow);
         }
     }
 }
 tableData();
+
